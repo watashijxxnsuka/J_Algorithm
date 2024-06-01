3e3bf3042c44e1971f1f9d9e0d53263818242a09
SeoJoon Han <165049341+watashijxxnsuka@users.noreply.github.com>
2024-06-01 13:12:02 +0900

    [Silver III] Title: 피보나치 함수, Time: 248 ms, Memory: 18684 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    static Integer[][] dp = new Integer[41][2];
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = sc.nextInt();
+
+        while(T-- > 0){
+            int N = sc.nextInt();
+            fibonacci(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+
+    }
+    static Integer[] fibonacci(int N) {
+        if(dp[N][0] == null || dp[N][1] == null) {
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        return dp[N];
+    }
+}
